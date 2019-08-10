61d5bece34fe9106061be26acac4bf33db333eff
Nikita Popov <ningen.tos@gmail.com>
2019-08-10 18:19:58 +0500

    Refactoring according to SRP

--- app.rb
+++ app.rb
@@ -7,8 +7,18 @@ class App
 
   def run
     loop do
-      n1, n2 = get_numbers
-      get_operation_result(Calculator.new(n1.to_i, n2.to_i), choose_operation)
+      puts "Please, put two numbers separated by space:"
+      n1, n2 = get_input
+
+      puts 'Please, choose one of the operations:'
+      puts '1. add'
+      puts '2. substract'
+      puts '3. multiply'
+      puts '4. divide'
+      operation = get_input[0]
+
+      puts 'The result of the operation is: ' +
+        get_operation_result(Calculator.new(n1.to_i, n2.to_i), operation)
     end
   end
 
@@ -18,34 +28,18 @@ class App
     gets.chomp.split /\s/
   end
 
-  def get_numbers
-    puts "Please, put two numbers separated by space:"
-    get_input
-  end
-
-  def choose_operation
-    puts 'Please, choose one of the operations:'
-    puts '1. add'
-    puts '2. substract'
-    puts '3. multiply'
-    puts '4. divide'
-
-    get_input[0]
-  end
-
   def get_operation_result(calculator, operation)
-    puts 'The result of the operation is: ' +
-             case operation
-             when '1'
-               calculator.add
-             when '2'
-               calculator.substract
-             when '3'
-               calculator.multiply
-             when '4'
-               calculator.divide
-             else
-               'You should choose an operation from the list above!'
-             end.to_s
+     case operation
+     when '1'
+       calculator.add
+     when '2'
+       calculator.substract
+     when '3'
+       calculator.multiply
+     when '4'
+       calculator.divide
+     else
+       'You should choose an operation from the list above!'
+     end.to_s
   end
 end
