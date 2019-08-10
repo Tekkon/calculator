cefd75657eb572fccf9340d2c739fb14c91932ab
Nikita Popov <ningen.tos@gmail.com>
2019-08-10 14:17:36 +0500

    Added class App and main.rb file

--- app.rb
+++ app.rb
@@ -1,36 +1,51 @@
 require_relative 'calculator'
 
-loop do
-  puts "Please, put two numbers separated by space:"
+class App
+  def self.run
+    loop do
+      choose_operation(create_calculator)
+    end
+  end
 
-  input = gets.chomp
-  *params = input.split /\s/
+  private
 
-  n1 = params[0].to_i
-  n2 = params[1].to_i
+  def self.get_chomp
+    input = gets.chomp
+    *params = input.split /\s/
+  end
 
-  calculator = Calculator.new(n1, n2)
+  def self.create_calculator
+    puts "Please, put two numbers separated by space:"
 
-  puts 'Please, choose one of the operations:'
-  puts '1. add'
-  puts '2. substract'
-  puts '3. multiply'
-  puts '4. divide'
+    params = get_chomp
 
-  input = gets.chomp
-  command, *params = input.split /\s/
+    n1 = params[0].to_i
+    n2 = params[1].to_i
 
-  puts 'The result of the operation is: ' +
-  case command
-  when '1'
-    calculator.add
-  when '2'
-    calculator.substract
-  when '3'
-    calculator.multiply
-  when '4'
-    calculator.divide
-  else
-    'You should choose an operation from the list above!'
-  end.to_s
+    Calculator.new(n1, n2)
+  end
+
+  def self.choose_operation(calculator)
+    puts 'Please, choose one of the operations:'
+    puts '1. add'
+    puts '2. substract'
+    puts '3. multiply'
+    puts '4. divide'
+
+    params = get_chomp
+
+    puts 'The result of the operation is: ' +
+             case params[0]
+             when '1'
+               calculator.add
+             when '2'
+               calculator.substract
+             when '3'
+               calculator.multiply
+             when '4'
+               calculator.divide
+             else
+               'You should choose an operation from the list above!'
+             end.to_s
+  end
 end
